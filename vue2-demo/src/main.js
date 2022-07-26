125e467f7b1952181e5d5dac3e0d7fe14b8bafbc
caojingfeng <caojingfengcn@outlook.com>
2022-07-26 17:09:59 +0800

    部分内容追加

--- vue2-demo/src/main.js
+++ vue2-demo/src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
-
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import '@/styles/index.scss'; // global css
+import 'normalize.css/normalize.css'; // A modern alternative to CSS resets
+// import ElementUI from 'element-ui';
+Vue.config.productionTip = false;
+// set ElementUI lang to EN
+// Vue.use(ElementUI);
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');

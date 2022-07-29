24ee01751c1895350d010d1d11bfaba3e1bec0e3
caojingfeng <caojingfengcn@outlook.com>
2022-07-29 12:51:29 +0800

    追加全局样式

--- vue2-demo/src/main.js
+++ vue2-demo/src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
-// import './styles/element-variables.scss';
+import 'normalize.css/normalize.css'; // A modern alternative to CSS resets
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import '@/styles/index.scss'; // global css
-import 'normalize.css/normalize.css'; // A modern alternative to CSS resets
-import ElementUI from 'element-ui';
 import '@/icons'; // icon
 import '@/styles/index.scss'; // global css
 Vue.config.productionTip = false;

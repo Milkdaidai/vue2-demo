7486ed50a3fba0af9585a6d2a1cfc4d88204d36f
caojingfeng <caojingfengcn@outlook.com>
2022-07-29 17:23:19 +0800

    i18n追加

--- vue2-demo/src/main.js
+++ vue2-demo/src/main.js
@@ -8,11 +8,23 @@ import store from './store';
 import '@/styles/index.scss'; // global css
 import '@/icons'; // icon
 import '@/styles/index.scss'; // global css
+import VueI18n from 'vue-i18n';
 Vue.config.productionTip = false;
+//i18n设置
+Vue.use(VueI18n);
+/*----------使用VueI18n-----------*/
+const i18n = new VueI18n({
+  locale: 'zh',
+  messages: {
+    // zh: require('./common/lang/zh'),
+    // en: require('./common/lang/en'),
+  },
+});
 // set ElementUI lang to EN
 Vue.use(ElementUI);
 new Vue({
   router,
   store,
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');

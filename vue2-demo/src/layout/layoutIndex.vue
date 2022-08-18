a35104305080fdada4b70973aab4d59be00c5f30
caojingfeng <caojingfengcn@outlook.com>
2022-08-18 15:08:02 +0800

    修改

--- vue2-demo/src/layout/layoutIndex.vue
+++ vue2-demo/src/layout/layoutIndex.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="layout-main">
+    <div class="menu-div"></div>
     <navbar />
   </div>
 </template>
@@ -12,4 +13,16 @@ export default {
   },
 };
 </script>
-<style></style>
+<style lang="scss">
+.layout-main {
+  width: 100%;
+  height: 100%;
+  .menu-div {
+    width: 20%;
+    height: 100%;
+    float: left;
+    border-right: 1px solid rgba(0, 21, 41, 0.08);
+  }
+}
+</style>
+>

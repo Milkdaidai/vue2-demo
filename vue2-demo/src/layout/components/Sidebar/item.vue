c22bd65480bf3db84d49099db74b1756d4b93560
caojingfeng <caojingfengcn@outlook.com>
2022-08-24 17:54:43 +0800

    render

--- vue2-demo/src/layout/components/Sidebar/item.vue
+++ vue2-demo/src/layout/components/Sidebar/item.vue
@@ -1,7 +1,22 @@
-<template>
-  <div></div>
-</template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isRed: true,
+    };
+  },
+
+  render(h) {
+    return h(
+      'div',
+      {
+        class: {
+          'is-red': this.isRed,
+        },
+      },
+      [h('p', '这是个render事例')]
+    );
+  },
+};
 </script>
 <style lang="scss" scoped></style>
